42e467ae24cd5e5ae96d9824e52f6354a9a2b414
TomiGraziani <grazianitfp@gmail.com>
2025-10-23 15:50:51 -0300

    desarrollo del backend de la aplicacion

---
+++ farmaya_app/farmaya_backend/accounts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ('email',)
+    list_display = ('email', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
