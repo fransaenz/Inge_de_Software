5d0b15ee2da5caa882c308b56538707c9405fa19
TomiGraziani <grazianitfp@gmail.com>
2025-11-05 09:53:37 -0300

    Actualizo modelos, serializers y urls del backend

--- farmaya_app/farmaya_backend/accounts/serializers.py
+++ farmaya_app/farmaya_backend/accounts/serializers.py
@@ -1,21 +1,60 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth import get_user_model
+from .models import Producto, Pedido, Receta
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=6)
+User = get_user_model()
+
+
+# -----------------------------
+# USUARIO
+# -----------------------------
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'first_name', 'last_name')
+        fields = ['id', 'email', 'password', 'tipo_usuario']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            tipo_usuario=validated_data.get('tipo_usuario', 'cliente')
+        )
         return user
 
-class UserSerializer(serializers.ModelSerializer):
+
+# -----------------------------
+# PRODUCTO
+# -----------------------------
+class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('id', 'email', 'first_name', 'last_name')
+        model = Producto
+        fields = ['id', 'nombre', 'precio', 'stock', 'requiere_receta']
+
+
+# -----------------------------
+# PEDIDO
+# -----------------------------
+class PedidoSerializer(serializers.ModelSerializer):
+    usuario = serializers.ReadOnlyField(source='usuario.email')
+    producto_nombre = serializers.ReadOnlyField(source='producto.nombre')
+
+    class Meta:
+        model = Pedido
+        fields = [
+            'id', 'usuario', 'producto', 'producto_nombre',
+            'cantidad', 'direccion_entrega', 'metodo_pago',
+            'farmacia', 'fecha', 'estado'
+        ]
+
+
+# -----------------------------
+# RECETA
+# -----------------------------
+class RecetaSerializer(serializers.ModelSerializer):
+    pedido_info = PedidoSerializer(source='pedido', read_only=True)
+
+    class Meta:
+        model = Receta
+        fields = ['id', 'pedido', 'pedido_info', 'imagen', 'fecha_subida']
